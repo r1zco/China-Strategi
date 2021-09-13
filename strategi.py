1a75cf26ae2bfb906007a0272067f4828c091743
r1zco <86164086+r1zco@users.noreply.github.com>
2021-09-12 21:36:19 -0400

    Update strategi.py

--- strategi.py
+++ strategi.py
@@ -1,10 +1,10 @@
-instrument{ name = "China r1zco",
-				short_name = "EC",
-				icon = "https://lh3.googleusercontent.com/a-/AOh14GhOMw-SBEHKf4dvwmVgwxUJaendPKluwo0itoR2MQ=s600-k-no-rp-mo",
-				overlay = true }
-				
-input_group ( "CALL", call_color = input( defaut = "green", type = input.color ) )
-input_group ( "PUT", call_color = input( defaut = "red", type = input.color ) )
+instrument{ name = "Chinesa r1zco", 
+                short_name = "EC", 
+                icon = "https://lh3.googleusercontent.com/a-/AOh14GhOMw-SBEHKf4dvwmVgwxUJaendPKluwo0itoR2MQ=s600-k-no-rp-mo",
+                overlay = true }
+                
+input_group { "CALL", call_color = input{ default = "green", type = input.color } }
+input_group { "PUT", put_color = input{ default = "red", type = input.color } }
 
 ssma_3 = ssma(close, 3)
 ssma_50 = ssma(close, 50)
@@ -16,22 +16,21 @@ drop_plot_value("PUT", current_bar_id)
 drop_plot_value("CALL", current_bar_id)
 
 plot_shape( (ssma_3 >= ssma_50 and ssma_3[1] < ssma_50[1] and ssma_3[2] < ssma_50[2] and MovAvarDev > MovAvarDev[1]),
-			"CALL",
-			shape,style.arrowup,
-			shape_size.huge,
-			call.color,
-			shape_location.belowbar,
-			0,
-			"CALL",
-			call_color)
-			
-			
-plot_shape( (ssma_3 <= ssma_50 and ssma_3[1] > ssma_50[1] and ssma_3[2] > ssma_50[2] and MovAvarDev < MovAvarDev[1]),
-			"PUT",
-			shape_style.arrowdown,
-			shape_size.huge,
-			put_color,
-			shape_location.abovebar,
-			0,
-			"PUT",
-			put_color)
+            "CALL",
+            shape_style.arrowup,
+            shape_size.huge,
+            call_color, 
+            shape_location.belowbar,
+            0,
+            "CALL",
+            call_color)
+            
+plot_shape( (ssma_3 <= ssma_50 and ssma_3[1] > ssma_50[1] and ssma_3[1] > ssma_50[1] and MovAvarDev < MovAvarDev[1]),
+            "PUT",
+            shape_style.arrowdown,
+            shape_size.huge,
+            put_color,
+            shape_location.abovebar,
+            0,
+            "PUT",
+            put_color)
